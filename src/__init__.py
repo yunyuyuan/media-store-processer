9d36e1ccf8666648ae903a47200265292dbd9ae0
yunyuyuan <me@yunyuyuan.net>
2023-06-21 13:51:40 +0800

    chore:use-python-to-rename

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,14 @@
 import argparse
+import datetime
 import subprocess
-from re import sub
+import re
+import os.path as path
+import os
 import shutil
+import traceback
+
+exif_path = '/usr/bin/vendor_perl/exiftool'
+app_log = None
 
 def get_args():
     parser = argparse.ArgumentParser(
@@ -15,59 +22,55 @@ def get_args():
     args = parser.parse_args()
     return args
 
-def exiftool(app_log = None):
-    args = get_args()
-    try:
-        for item in [
-            {
-                'tag': 'CreateDate',
-                'cond': '',
-                'format': '%Y/%m/%Y-%m-%d_%H-%M-%S.%%e',
-            },{
-                'tag': 'FileModifyDate',
-                'cond': 'not $CreateDate',
-                'format': '%Y/%m/%Y-%m-%d_%H-%M-%S%%-c.%%e',
-            },
-        ]:
-            process = subprocess.Popen(_generate_cmd(
-                    src=args.src,
-                    dist=args.dist,
-                    tag=item['tag'],
-                    format=item['format'],
-                    cond=item['cond'],
-                    copy=args.copy,
-                    recursive=args.recursive
-                ), 
-                shell=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                universal_newlines=True)
-            code = process.wait()
-            out, err = process.communicate()
-            log = f"[out of <{item['tag']}>]:\n{err}\n{out}"
-            if app_log:
-                app_log.info(log)
-            else:
-                print(log)
-    except Exception as e:
-        if app_log:
-            app_log.error('Python error:\n' + str(e))
-        else:
-            print(str(e))
+def print_log(s):
+    global app_log
+    if app_log:
+        app_log.info(s)
+    else:
+        print(s)
 
-def _generate_cmd(src: str, dist: str, tag: str, format: str, cond: str, copy: bool, recursive: bool):
-    #exif_bin = shutil.which('exiftool')
-    exif_bin = '/usr/bin/vendor_perl/exiftool'
-    #if not exif_bin:
-        #raise(Exception('exiftool binary not found!'))
-    cmd = f"{exif_bin} '-FileName<{tag}'"
-    cmd += f" -d {sub('/*$', '', dist)}/"
-    cmd += format
-    if copy:
-        cmd += ' -o .'
-    if recursive: 
-        cmd += ' -r'
-    if cond:
-        cmd += f" -if '{cond}'"
-    cmd += ' ' + src
-    return cmd
+def run_command(command):
+    process = subprocess.Popen(command, 
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True)
+    code = process.wait()
+    out, err = process.communicate()
+    log = f"[RUNNING]: {command}\nERROR: {err}\nOUTPUT: {out}"
+    print_log(log)
+    return out
+
+def exiftool(_app_log = None):
+    global app_log
+    app_log = _app_log
+    args = get_args()
+    src = args.src
+    dist = args.dist
+    try:
+        for filename in os.listdir(src):
+            file_src = path.join(src, filename)
+            file_src_quoted = "'" + file_src.replace("'", "'\\''") + "'"
+            file_extension = os.path.splitext(filename)[1]
+
+            # get available time
+            available_time = run_command(f'{exif_path} -s -time:all {file_src_quoted}')
+            time_key = "CreateDate" if re.search('CreateDate', available_time) else "FileModifyDate"
+            # get the time
+            original_format = '%Y:%m:%d %H:%M:%S'
+            create_date = run_command(f'{exif_path} -d "{original_format}" -{time_key} -s3 {file_src_quoted}').strip()
+            new_format = '%Y/%m/%Y-%m-%d_%H-%M-%S'
+            formatted_date = datetime.datetime.strptime(create_date, original_format).strftime(new_format)
+            
+            index = 0
+            while 1:
+                file_dist = path.join(dist, f"{formatted_date}{f'-{index}' if index else ''}{file_extension}")
+                if path.exists(file_dist):
+                    index += 1
+                else:
+                    # os.rename(file_src, file_dist)
+                    shutil.move(file_src, file_dist)
+                    print(f'[MOVE] {file_src} [TO] {file_dist}')
+                    break
+    except Exception as e:
+        print_log('[Python error]: \n' + traceback.format_exc())
